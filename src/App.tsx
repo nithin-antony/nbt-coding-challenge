ab76a612c6173d325228b7bdb0a07a0ba429e66a
Nithin Antony <nithin.antony@outlook.in>
2022-10-06 12:02:48 +0530

    adding material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 import './style.css'
 
 export const App = () => {
   return (
     <>
       <h1>dashboard app</h1>
+      <Button variant="text">Text</Button>
     </>
   )
 }
